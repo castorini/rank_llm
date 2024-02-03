ece02265bda24c9642c7618bce2afaa21524fbb3
Richard Fan <frqrichard@gmail.com>
2024-02-03 16:50:17 -0500

    added init files to make testing scripts discoverable

--- test/run_all.sh
+++
@@ -1,8 +0,0 @@
-echo "Running tests..."
-tests=("retrieve/test_PyseriniRetriever.py" "rerank/test_RankListwiseOSLLM.py" "rerank/test_SafeOpenai.py")
-for test in "${tests[@]}"
-do
-    echo "Running $test tests..."
-    python3 "$test"
-    echo 
-done
