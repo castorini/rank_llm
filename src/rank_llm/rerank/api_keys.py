f4f77c87061664566ce946252d310d889a93f413
sahel <sahel.sharifi@gmail.com>
2024-02-03 13:42:25 -0500

    update the demo

---
+++ src/rank_llm/rerank/api_keys.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+from typing import Dict
+
+
+def get_openai_api_key() -> str:
+    load_dotenv(dotenv_path=f".env.local")
+    return os.getenv("OPEN_AI_API_KEY")
+
+
+def get_azure_openai_args() -> Dict[str, str]:
+    load_dotenv(dotenv_path=f".env.local")
+    azure_args = {
+        "api_type": "azure",
+        "api_version": os.getenv("AZURE_OPENAI_API_VERSION"),
+        "api_base": os.getenv("AZURE_OPENAI_API_BASE"),
+    }
+
+    # Sanity check
+    assert all(
+        list(azure_args.values())
+    ), "Ensure that `AZURE_OPENAI_API_BASE`, `AZURE_OPENAI_API_VERSION` are set"
+    return azure_args
