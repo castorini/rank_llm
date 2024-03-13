457db475552797857e7d7656c130479f4f27330d
Charlie Liu <c596liu@uwaterloo.ca>
2024-03-13 20:20:36 +0000

    Added Script to generate info of retrevial cache

---
+++ src/rank_llm/scripts/query_cache.py
@@ -0,0 +1,71 @@
+import os
+from pyserini.util import *
+from urllib.parse import urlparse
+import urllib.request
+import json
+
+def no_bool_convert(pairs):
+    return {k: str(v).casefold()
+            if isinstance(v, bool) else v for k, v in pairs}
+
+def parse_file_info(file_name, file_url, save_dir):
+    #download file
+    local_file_path = download_url(file_url, save_dir, local_filename=file_name, verbose=True)
+
+    # Compute MD5 
+    md5 = compute_md5(local_file_path)
+
+    # Get file size
+    size = os.path.getsize(local_file_path)
+
+    # Remove the downloaded file
+    os.remove(local_file_path)
+
+    # Placeholder description
+    description = "Sample description"
+
+    return description, md5, size
+
+# Save directory
+def get_repo_info(repo_url, save_dir):
+    parsed_url = urlparse(repo_url)
+    path_parts = parsed_url.path.strip('/').split('/')
+    if len(path_parts) < 2:
+        raise ValueError("Invalid GitHub repository URL")
+
+    owner, repo = path_parts[:2]
+    api_url = f"https://api.github.com/repos/{owner}/{repo}/git/trees/main?recursive=1"
+    with urllib.request.urlopen(api_url) as response:
+        data = json.loads(response.read().decode())
+
+    query_info = {}
+    for file in data.get('tree', []):
+        if file['type'] == 'blob':
+            file_name = file['path']
+            file_url = f"https://github.com/{owner}/{repo}/raw/main/{file_name}"
+            description, md5, size = parse_file_info(file_name, file_url, save_dir)
+            query_info[file_name.rsplit('/', 1)[-1]] = {
+                "description": description,
+                "urls": [file_url],
+                "md5": md5,
+                "size (bytes)": size,
+                "downloaded": False
+            }
+    return query_info
+
+repo_url = "https://github.com/castorini/rank_llm_data/tree/main/retrieve_results"
+save_dir = "retrieve_results/SPLADE_P_P_ENSEMBLE_DISTIL" 
+info = get_repo_info(repo_url, save_dir)
+info_json = json.dumps(info, indent=4)
+
+with open('repo_info.txt', 'w') as f:
+    f.write(f'QUERY_INFO = {info_json}\n')
+
+with open('repo_info.txt', 'r') as f:
+    data = f.read()
+    data = data.replace('false', 'False')
+
+with open('repo_info.py', 'w') as f:
+    f.write(data)
+
+os.remove('repo_info.txt')
