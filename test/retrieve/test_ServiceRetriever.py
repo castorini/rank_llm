9874ae759643ef0e48490fb346311d8379a78f84
Ryan Nguyen <ryan.nguyen@uwaterloo.ca>
2024-05-18 13:23:21 +0000

    update serviceRetriever test

--- test/retrieve/test_ServiceRetriever.py
+++ test/retrieve/test_ServiceRetriever.py
@@ -7,11 +7,10 @@ from rank_llm.data import Request, Query, Candidate
 class TestServiceRetriever(unittest.TestCase):
     def test_from_datatest_with_prebuilt_index(self):
 
-        response = ServiceRetriever.from_dataset_with_prebuilt_index(request=Request(query=Query(text="hello",qid="1")), dataset_name="msmarco-v2.1-doc")
+        response = ServiceRetriever.from_dataset_with_prebuilt_index(request=Request(query=Query(text="hello",qid="1234")), dataset_name="msmarco-v2.1-doc",k=20)
 
-        assert(len(response.candidates)==10)
+        assert(len(response.candidates)==20)
         assert(type(response.candidates[0]) == Candidate)
-        assert(response.query == Query(text='hello', qid=''))
+        assert(response.query == Query(text='hello', qid='1234'))
 
     # TODO def test_retrieve_and_rerank_interactive(self):
-        
