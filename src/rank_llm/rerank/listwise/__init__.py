56905a22f240938c8d449610326f51ccaa8c92e6
Ryan Nguyen <ryan.nguyen@uwaterloo.ca>
2024-07-26 06:06:11 +0000

    pre-commit

--- src/rank_llm/rerank/listwise/__init__.py
+++ src/rank_llm/rerank/listwise/__init__.py
@@ -1,17 +1,16 @@
+from .gpt import *
 from .listwise_rankllm import ListwiseRankLLM, PromptMode
 from .rank_listwise_os_llm import RankListwiseOSLLM
 from .vicuna_reranker import VicunaReranker
 from .zephyr_reranker import ZephyrReranker
 
-from .gpt import *
-
 __all__ = [
-    'ListwiseRankLLM',
-    'PromptMode',
-    'RankListwiseOSLLM',
-    'VicunaReranker',
-    'ZephyrReranker',
-    'SafeOpenai',
-    'get_azure_openai_args',
-    'get_openai_api_key',
+    "ListwiseRankLLM",
+    "PromptMode",
+    "RankListwiseOSLLM",
+    "VicunaReranker",
+    "ZephyrReranker",
+    "SafeOpenai",
+    "get_azure_openai_args",
+    "get_openai_api_key",
 ]
--- src/rank_llm/rerank/listwise/gpt/__init__.py
+++ src/rank_llm/rerank/listwise/gpt/__init__.py
@@ -1,9 +1,9 @@
-from .rank_gpt import SafeOpenai, CompletionMode
 from .api_keys import get_azure_openai_args, get_openai_api_key
+from .rank_gpt import CompletionMode, SafeOpenai
 
 __all__ = [
-    'SafeOpenai',
-    'CompletionMode',
-    'get_azure_openai_args',
-    'get_openai_api_key',
+    "SafeOpenai",
+    "CompletionMode",
+    "get_azure_openai_args",
+    "get_openai_api_key",
 ]
