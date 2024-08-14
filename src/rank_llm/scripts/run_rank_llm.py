501a9a950fafc4b92e97939123c0bb66906adffd
Ryan Nguyen <ryan.nguyen@uwaterloo.ca>
2024-08-14 13:12:56 +0000

    removed double batch size in run_rank_llm

--- src/rank_llm/scripts/run_rank_llm.py
+++ src/rank_llm/scripts/run_rank_llm.py
@@ -38,7 +38,6 @@ def main(args):
     window_size = args.window_size
     system_message = args.system_message
     vllm_batched = args.vllm_batched
-    batch_size = args.batch_size
 
     _ = retrieve_and_rerank(
         model_path=model_path,
@@ -63,7 +62,6 @@ def main(args):
         step_size=step_size,
         system_message=system_message,
         vllm_batched=vllm_batched,
-        batch_size=batch_size,
     )
 
 
@@ -176,12 +174,6 @@ if __name__ == "__main__":
         "--vllm_batched",
         action="store_true",
         help="whether to run the model in batches",
-    )
-    parser.add_argument(
-        "--batch_size",
-        default=-1,
-        help="batch size of the non vllm-determined-batch-size models. -1 means not allowed be in batch",
-        type=int,
     )
     args = parser.parse_args()
     main(args)
