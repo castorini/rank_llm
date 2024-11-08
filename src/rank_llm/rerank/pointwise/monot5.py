7cb4c5140a753b3ce09ea17e3acae22e5cb3f214
Yuv-sue1005 <168255174+Yuv-sue1005@users.noreply.github.com>
2024-11-08 13:38:01 -0500

    Fix false logits for mt5

--- src/rank_llm/rerank/pointwise/monot5.py
+++ src/rank_llm/rerank/pointwise/monot5.py
@@ -10,6 +10,29 @@ from rank_llm.rerank.pointwise.pointwise_rankllm import PointwiseRankLLM
 
 logger = logging.getLogger(__name__)
 
+PREDICTION_TOKENS = {
+        'castorini/monot5-base-msmarco':             ['▁false', '▁true'],
+        'castorini/monot5-base-msmarco-10k':         ['▁false', '▁true'],
+        'castorini/monot5-large-msmarco':            ['▁false', '▁true'],
+        'castorini/monot5-large-msmarco-10k':        ['▁false', '▁true'],
+        'castorini/monot5-base-med-msmarco':         ['▁false', '▁true'],
+        'castorini/monot5-3b-med-msmarco':           ['▁false', '▁true'],
+        'castorini/monot5-3b-msmarco-10k':           ['▁false', '▁true'],
+        'unicamp-dl/mt5-base-en-msmarco':            ['▁no'   , '▁yes'],
+        'unicamp-dl/ptt5-base-pt-msmarco-10k-v2':    ['▁não'  , '▁sim'],
+        'unicamp-dl/ptt5-base-pt-msmarco-100k-v2':   ['▁não'  , '▁sim'],
+        'unicamp-dl/ptt5-base-en-pt-msmarco-100k-v2':['▁não'  , '▁sim'],
+        'unicamp-dl/mt5-base-en-pt-msmarco-v2':      ['▁no'   , '▁yes'],
+        'unicamp-dl/mt5-base-mmarco-v2':             ['▁no'   , '▁yes'],
+        'unicamp-dl/mt5-base-en-pt-msmarco-v1':      ['▁no'   , '▁yes'],
+        'unicamp-dl/mt5-base-mmarco-v1':             ['▁no'   , '▁yes'],
+        'unicamp-dl/ptt5-base-pt-msmarco-10k-v1':    ['▁não'  , '▁sim'],
+        'unicamp-dl/ptt5-base-pt-msmarco-100k-v1':   ['▁não'  , '▁sim'],
+        'unicamp-dl/ptt5-base-en-pt-msmarco-10k-v1': ['▁não'  , '▁sim'],
+        'unicamp-dl/mt5-3b-mmarco-en-pt':            ['▁'  , '▁true'],
+        'unicamp-dl/mt5-13b-mmarco-100k':            ['▁', '▁true'],
+        }
+
 
 class MonoT5(PointwiseRankLLM):
     def __init__(
@@ -38,6 +61,17 @@ class MonoT5(PointwiseRankLLM):
             self._llm = T5ForConditionalGeneration.from_pretrained(model).to(self._device)
         self._context_size = context_size
 
+    @staticmethod
+    def get_prediction_tokens(pretrained_model_name_or_path: str, tokenizer):
+        if pretrained_model_name_or_path in PREDICTION_TOKENS:
+            token_false, token_true = PREDICTION_TOKENS[pretrained_model_name_or_path]
+            token_false_id = tokenizer.get_vocab()[token_false]
+            token_true_id  = tokenizer.get_vocab()[token_true]
+            return token_false_id, token_true_id
+        else:
+            raise Exception(f"We don't know the indexes for the non-relevant/relevant tokens for\
+                    the checkpoint {pretrained_model_name_or_path} and you did not provide any.")
+        pass
     def run_llm_batched(
         self,
         prompts: List[str],
@@ -76,10 +110,15 @@ class MonoT5(PointwiseRankLLM):
         ]
 
         for logit_tensor in batch_logits[0]:
-            truth_logit = logit_tensor[1176]
-            false_logit = logit_tensor[6136]
-            score = math.exp(truth_logit) / (
-                math.exp(truth_logit) + math.exp(false_logit)
+            token_false_id, token_true_id = self.get_prediction_tokens(self._model, self._tokenizer)
+            pos_index = logit_tensor[token_true_id]
+            neg_index = logit_tensor[token_false_id]
+            #wanted_logit = logit_tensor.find("")
+            #print(f"current False_logit is {neg_index}")
+            #print(f"wanted false logit can be found at {wanted_logit}")
+
+            score = math.exp(pos_index) / (
+                math.exp(pos_index) + math.exp(neg_index)
             )
             all_scores.append(score)
             all_output_token_counts.append(self.num_output_tokens)
@@ -87,6 +126,8 @@ class MonoT5(PointwiseRankLLM):
         all_outputs.extend(batch_outputs)
 
         return all_outputs, all_output_token_counts, all_scores
+
+
 
     def run_llm(self, prompt: str) -> Tuple[str, int, float]:
         gen_cfg = GenerationConfig.from_model_config(self._llm.config)
