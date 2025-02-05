6dbd88a8f8e813c2918bc537771aefa21e254811
Sahel Sharifymoghaddam <sahel.sharifymoghaddam@uwaterloo.ca>
2025-02-05 01:30:16 -0500

    Polish pointwise (#171)
    * populate exect summary with pointwise if needed, add demo for monot5, black reformat
    
    
    Co-authored-by: Ryan Nguyen <ryan.nguyen@uwaterloo.ca>

---
+++ src/rank_llm/demo/rerank_monot5.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from pathlib import Path
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.dirname(SCRIPT_DIR)
+parent = os.path.dirname(parent)
+sys.path.append(parent)
+
+from rank_llm.data import DataWriter
+from rank_llm.rerank import Reranker
+from rank_llm.rerank.pointwise.monot5 import MonoT5
+from rank_llm.retrieve.retriever import Retriever
+
+dataset = "dl19"
+requests = Retriever.from_dataset_with_prebuilt_index(dataset, k=100)
+monot5_agent = MonoT5("castorini/monot5-3b-msmarco-10k")
+m_reranker = Reranker(monot5_agent)
+kwargs = {"populate_exec_summary": True}
+rerank_results = m_reranker.rerank_batch(requests, **kwargs)
+print(rerank_results)
+
+# write rerank results
+writer = DataWriter(rerank_results)
+Path(f"demo_outputs/").mkdir(parents=True, exist_ok=True)
+writer.write_in_json_format(f"demo_outputs/rerank_results.json")
+writer.write_in_trec_eval_format(f"demo_outputs/rerank_results.txt")
+writer.write_ranking_exec_summary(f"demo_outputs/ranking_execution_summary.json")
