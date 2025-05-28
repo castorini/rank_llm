e6d56888330a5001243945c61e3b7898a39fe4ee
Daniel Guo <82596509+clides@users.noreply.github.com>
2025-05-28 17:50:02 -0400

    added invocation history argument (#226)

--- src/rank_llm/scripts/run_rank_llm.py
+++ src/rank_llm/scripts/run_rank_llm.py
@@ -37,6 +37,7 @@ def main(args):
     stride = args.stride
     window_size = args.window_size
     system_message = args.system_message
+    populate_invocations_history = args.populate_invocations_history
     use_logits = args.use_logits
     use_alpha = args.use_alpha
     sglang_batched = args.sglang_batched
@@ -64,6 +65,7 @@ def main(args):
         window_size=window_size,
         stride=stride,
         system_message=system_message,
+        populate_invocations_history=populate_invocations_history,
         use_logits=use_logits,
         use_alpha=use_alpha,
         sglang_batched=sglang_batched,
@@ -176,6 +178,11 @@ if __name__ == "__main__":
         type=str,
         default="You are RankLLM, an intelligent assistant that can rank passages based on their relevancy to the query.",
         help="the system message used in prompts",
+    )
+    parser.add_argument(
+        "--populate_invocations_history",
+        action="store_true",
+        help="write a file with the prompts and raw responses from LLM",
     )
     infer_backend_group = parser.add_mutually_exclusive_group()
     parser.add_argument(
