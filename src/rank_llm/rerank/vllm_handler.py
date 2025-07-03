8c2171ff3351181704cc7d49c52e1ff1cd64bb21
clides <daniel168.guo@gmail.com>
2025-07-03 10:59:05 -0400

    created vllm handler class and implemented it in rank_listwise_os_llm

---
+++ src/rank_llm/rerank/vllm_handler.py
@@ -0,0 +1,56 @@
+from typing import Any, Dict, List, Optional
+
+import vllm
+from vllm.outputs import RequestOutput
+
+
+class vLLMHandler:
+    def __init__(
+        self,
+        model: str,
+        download_dir: str,
+        enforce_eager: bool,
+        max_logprobs: int,
+        tensor_parallel_size: int,
+        gpu_memory_utilization: float,
+        **kwargs: Any,
+    ):
+        self._vllm = vllm.LLM(
+            model=model,
+            download_dir=download_dir,
+            enforce_eager=enforce_eager,
+            max_logprobs=max_logprobs,
+            tensor_parallel_size=tensor_parallel_size,
+            gpu_memory_utilization=gpu_memory_utilization,
+        )
+        self._tokenizer = self._llm.get_tokenizer()
+
+        if "rank_vicuna" in model:
+            setattr(
+                self._tokenizer,
+                "chat_template",
+                """{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}
+                    {% for message in messages %}{% if not loop.first %}{% endif %}{% if message['role'] == 'system' %}{{ message['content'] + ' ' }}{% elif message['role'] == 'user' %}{{ 'USER: ' + message['content'] + ' ' }}{% elif message['role'] == 'assistant' %}{{ 'ASSISTANT: ' + message['content'] + '</s>' }}{% endif %}{% endfor %}{% if add_generation_prompt %}{{ 'ASSISTANT:' }}{% endif %}""",
+            )
+
+    def get_tokenizer(self) -> vllm.Tokenizer:
+        return self._tokenizer
+
+    def generate_output(
+        self,
+        prompts: List[str | List[Dict[str, str]]],
+        min_tokens: int,
+        max_tokens: int,
+        temperature: float,
+        logprobs: Optional[int] = None,
+        **kwargs: Any,
+    ) -> List[RequestOutput]:
+        # TODO: Implement rest of vllm arguments (from kwargs) in the future if necessary
+        sampling_params = vllm.SamplingParams(
+            min_tokens=min_tokens,
+            max_tokens=max_tokens,
+            temperature=temperature,
+            logprobs=logprobs,
+        )
+
+        return self._llm.generate(prompts, sampling_params)
