22a53d911e1f65af6dcf99fa5a16efd1a18078f2
Sahel Sharifymoghaddam <sahel.sharifymoghaddam@uwaterloo.ca>
2025-07-22 15:29:53 -0400

    Bug fixes for the experimental_results demo to work (#274)
    * Bug fixes for the experimental_results demo to work
    * update the analyzer unittest to pass the regex the same way we would read from the YAML files

--- test/analysis/test_response_analyzer.py
+++ test/analysis/test_response_analyzer.py
@@ -17,16 +17,16 @@ class TestResponseAnalyzer(unittest.TestCase):
                         response="[1] > [2] > [3]",
                         input_token_count=100,
                         output_token_count=50,
-                        output_validation_regex=r"^\[\d+\]( > \[\d+\])*$",
-                        output_extraction_regex=r"\[(\d+)\]",
+                        output_validation_regex='r"^\[\d+\]( > \[\d+\])*$"',
+                        output_extraction_regex='r"\[(\d+)\]"',
                     ),
                     InferenceInvocation(
                         prompt="I will provide you with 2 passages: [1]Test, [2]Test",
                         response="[2] > [1]",
                         input_token_count=80,
                         output_token_count=40,
-                        output_validation_regex=r"^\[\d+\]( > \[\d+\])*$",
-                        output_extraction_regex=r"\[(\d+)\]",
+                        output_validation_regex='r"^\[\d+\]( > \[\d+\])*$"',
+                        output_extraction_regex='r"\[(\d+)\]"',
                     ),
                 ],
             ),
@@ -39,8 +39,8 @@ class TestResponseAnalyzer(unittest.TestCase):
                         response="[4] > [3] > [2] > [1]",
                         input_token_count=120,
                         output_token_count=60,
-                        output_validation_regex=r"^\[\d+\]( > \[\d+\])*$",
-                        output_extraction_regex=r"\[(\d+)\]",
+                        output_validation_regex='r"^\[\d+\]( > \[\d+\])*$"',
+                        output_extraction_regex='r"\[(\d+)\]"',
                     )
                 ],
             ),
@@ -59,6 +59,14 @@ class TestResponseAnalyzer(unittest.TestCase):
         self.assertEqual(len(num_passages), 3, "Should have 3 num_passages")
         self.assertEqual(
             num_passages, [3, 2, 4], "Num passages should match expected values"
+        )
+        self.assertEqual(
+            output_validation_regex,
+            self.mock_results[0].invocations_history[0].output_validation_regex,
+        )
+        self.assertEqual(
+            output_extraction_regex,
+            self.mock_results[0].invocations_history[0].output_extraction_regex,
         )
 
 
