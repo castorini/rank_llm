6909695685ee6d77925effdc104fdf4b00e12421
sahel-sh <sahel.sharifi@gmail.com>
2025-09-03 23:12:47 +0000

    add reasonrank with a demo

--- src/rank_llm/rerank/listwise/rank_listwise_os_llm.py
+++ src/rank_llm/rerank/listwise/rank_listwise_os_llm.py
@@ -180,7 +180,7 @@ class RankListwiseOSLLM(ListwiseRankLLM):
         )
 
         # reranking using vllm or sglang
-        if len(set([len(req.candidates) for req in requests])) != 1:
+        if self._batch_size > 1 and len(set([len(req.candidates) for req in requests])) != 1:
             raise ValueError("Batched requests must have the same number of candidates")
 
         return self.sliding_windows_batched(
@@ -260,7 +260,7 @@ class RankListwiseOSLLM(ListwiseRankLLM):
                     prompts=prompts,
                     min_tokens=self.num_output_tokens(current_window_size),
                     max_tokens=(
-                        self._reasoning_token_budget
+                        self._reasoning_token_budget + self.num_output_tokens(current_window_size)
                         if self._is_thinking
                         else self.num_output_tokens(current_window_size)
                     ),
